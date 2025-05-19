b16f4c7cb15037c0775dd32944d8420979376d76
VoxeveR <piotrzietek01@gmail.com>
2025-05-19 16:53:31 +0200

    refactor: update package path

--- src/main/java/pl/edu/pk/student/kittysecurity/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/exception/GlobalExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import pl.edu.pk.student.kittysecurity.dto.ErrorResponseDto;
+import pl.edu.pk.student.kittysecurity.dto.error.ErrorResponseDto;
 import pl.edu.pk.student.kittysecurity.exception.custom.UserAlreadyExistsException;
 import pl.edu.pk.student.kittysecurity.exception.custom.UserNotFoundException;
 
