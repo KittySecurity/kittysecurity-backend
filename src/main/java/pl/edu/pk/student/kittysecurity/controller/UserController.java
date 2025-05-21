784ca74e4796c0ba997f8f15f5f42810a0a27a40
VoxeveR <piotrzietek01@gmail.com>
2025-05-21 15:38:18 +0200

    feat: add user put method handler

--- src/main/java/pl/edu/pk/student/kittysecurity/controller/UserController.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import pl.edu.pk.student.kittysecurity.dto.other.StatusResponseDto;
 import pl.edu.pk.student.kittysecurity.dto.user.DeleteUserRequestDto;
 import pl.edu.pk.student.kittysecurity.dto.user.UserResponseDto;
+import pl.edu.pk.student.kittysecurity.dto.user.UserUpdateRequestDto;
+import pl.edu.pk.student.kittysecurity.dto.user.UserUpdateResponseDto;
 import pl.edu.pk.student.kittysecurity.services.UserService;
 
 @RestController
@@ -22,6 +24,11 @@ public class UserController {
     @GetMapping("")
     public ResponseEntity<UserResponseDto> getUserData(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwtToken){
         return userService.getUserDataByJwt(jwtToken);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<UserUpdateResponseDto> updateUser(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwtToken, @RequestBody @Valid UserUpdateRequestDto request){
+        return userService.updateUserByJwt(jwtToken, request);
     }
 
     @DeleteMapping("")
