9dbd5a5332b740db2a52a30bb5f899aeca89f0c3
Alex Kalakutsky <shurik.kalakutsky@gmail.com>
2025-05-22 13:31:32 +0300

    fix: add missing PasswordEntry file

--- src/main/java/pl/edu/pk/student/kittysecurity/entity/PasswordEntry.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/entity/PasswordEntry.java
@@ -1,4 +1,31 @@
 package pl.edu.pk.student.kittysecurity.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class PasswordEntry {
+    @Id
+    @Column(nullable = false, unique = true)
+    @GeneratedValue
+    private UUID entryId;
+
+    @Column(nullable = false)
+    private UUID userId;
+
+    private String serviceName;
+
+    private String url;
+
+    private String login;
+
+    private String passwordEncrypted;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
