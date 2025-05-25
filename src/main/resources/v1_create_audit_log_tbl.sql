b94ca7fca81c7db94bdfea25875ef7da6e163a92
VoxeveR <piotrzietek01@gmail.com>
2025-05-25 14:38:41 +0200

    feat: add flyway migrations

--- src/main/resources/v1_create_audit_log_tbl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE AuditLog (
-    log_id BIGINT PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    action VARCHAR(255) NOT NULL,
-    timestamp TIMESTAMP NOT NULL,
-    ip_address VARCHAR(45) NOT NULL,
-    CONSTRAINT fk_audit_user FOREIGN KEY (user_id) REFERENCES User(user_id)
-);
