2ffa378e8611de3c2887e8bbd7f8c6c16fe67d52
VoxeveR <piotrzietek01@gmail.com>
2025-05-28 23:57:44 +0200

    fix: add not null to create password request fields

--- src/main/java/pl/edu/pk/student/kittysecurity/dto/password/CreatePasswordRequestDto.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/dto/password/CreatePasswordRequestDto.java
@@ -1,6 +1,7 @@
 package pl.edu.pk.student.kittysecurity.dto.password;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
@@ -10,16 +11,21 @@ import lombok.*;
 @AllArgsConstructor
 public class CreatePasswordRequestDto {
 
+    @NotNull
     private String url;
 
+    @NotNull
     private String login;
 
+    @NotNull
     @JsonProperty("name")
     private String serviceName;
 
+    @NotNull
     @JsonProperty("encrypted")
     private String password;
 
+    @NotNull
     @JsonProperty("IV")
     private String Iv;
 }
