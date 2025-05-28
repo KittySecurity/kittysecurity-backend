bbaa5baa9c2243c71d8f5f16a01735a6c2dc52b9
VoxeveR <piotrzietek01@gmail.com>
2025-05-28 23:58:26 +0200

    feat: add find password by User

--- src/main/java/pl/edu/pk/student/kittysecurity/repository/PasswordEntryRepository.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/repository/PasswordEntryRepository.java
@@ -2,8 +2,12 @@ package pl.edu.pk.student.kittysecurity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.edu.pk.student.kittysecurity.entity.PasswordEntry;
+import pl.edu.pk.student.kittysecurity.entity.User;
+
+import java.util.List;
 
 
 public interface PasswordEntryRepository extends JpaRepository<PasswordEntry, Integer> {
 
+    List<PasswordEntry> findByUser(User user);
 }
