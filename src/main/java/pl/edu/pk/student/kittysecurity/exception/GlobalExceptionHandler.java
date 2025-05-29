17212a24d2cad614bba9500d671570da72404336
VoxeveR <piotrzietek01@gmail.com>
2025-05-29 18:12:17 +0200

    fix: remove unused imports

--- src/main/java/pl/edu/pk/student/kittysecurity/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/edu/pk/student/kittysecurity/exception/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package pl.edu.pk.student.kittysecurity.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
