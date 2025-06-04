48d71d3a075452768b1259170c112890561c23b2
VoxeveR <piotrzietek01@gmail.com>
2025-06-04 15:55:22 +0200

    feat: add passwordsettingsupdaterequestdto

---
+++ src/main/java/pl/edu/pk/student/kittysecurity/dto/password/settings/PasswordSettingsUpdateRequestDto.java
@@ -0,0 +1,44 @@
+package pl.edu.pk.student.kittysecurity.dto.password.settings;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class PasswordSettingsUpdateRequestDto {
+    @JsonProperty("length")
+    @Size(min = 8, message = "Generated password should be at least 8 characters long")
+    @Size(max = 128, message = "Generated password should be not longer than 128 characters")
+    private Integer passwordLength;
+
+    @JsonProperty("minNumbers")
+    @Size(min = 1, message = "Password should contain at least 1 number")
+    @Size(max = 9, message = "Password can contain 9 numbers at maximum")
+    private Integer minNumOfDigits;
+
+    @JsonProperty("minSpecials")
+    @Size(min = 1, message = "Password should contain at least 1 special character")
+    @Size(max = 9, message = "Password can contain 9 special characters at maximum")
+    private Integer minNumOfSpecChars;
+
+    @JsonProperty("lowercase")
+    private Boolean hasLowercase;
+
+    @JsonProperty("uppercase")
+    @Size(message = "Username should be at least 4 characters long")
+    private Boolean hasUppercase;
+
+    @JsonProperty("specials")
+    @Size(message = "Username should be at least 4 characters long")
+    private Boolean hasSpecial;
+
+    @JsonProperty("numbers")
+    @Size(message = "Username should be at least 4 characters long")
+    private Boolean hasDigits;
+
+}
